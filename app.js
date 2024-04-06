68ab779cad5483a15a92642c0b6b0ee9b679d2db
Krishnagokul1305 <krishnagokul1729@gmail.com>
2024-04-06 09:15:22 +0530

    basic structure created

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-const markup=` <div class="col">
-<div class="card">
-    <img src="" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title"></h5>
-    <p class="card-text"></p>
-    </div>
-</div>
-</div>`
+const markup=` `
 
-async function fetchApi(){
-   const response=await fetch('https://api.unsplash.com/search/photos?page=1&query=office')
+
+class ImageGenerator{
+    _data;
+    _parentElement=document.querySelector('.result-container')
+    render(){
+      
+    }
+    fetchApi(qurey){
+        fetch('https://api.unsplash.com/search/photos?page=3&query=office&client_id=cwAj67RC0CDx_aEw7SS0pzt93AZL9neDIP9k-8CroMI')
+        .then(res=>res.json())
+        .then(data=>this._data=data)
+        console.log(this._data)
+    }
+    generateMarkup(){
+        this._parentElement.innerHTML='';
+        return `<div class="col">
+        <div class="card">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title"></h5>
+            <p class="card-text"></p>
+            </div>
+        </div>
+        </div>`
+    }
+    renderSpinner(){
+        
+    }
 }
-fetchApi()
+
+const generator=new ImageGenerator()
+console.log(generator)
+generator.fetchApi()
